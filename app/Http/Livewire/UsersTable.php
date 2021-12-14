e5a5674fb67e14b19737bc56c04b225f06dc6c07
cugarte94 <cugarte@guiasyscoutschile.cl>
2021-12-14 16:09:34 -0300

    updates

--- app/Http/Livewire/UsersTable.php
+++ app/Http/Livewire/UsersTable.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Livewire\Component;
 use Mediconesystems\LivewireDatatables\Column;
 use Mediconesystems\LivewireDatatables\NumberColumn;
 use Mediconesystems\LivewireDatatables\BooleanColumn;
@@ -73,5 +75,24 @@ class UsersTable extends LivewireDatatable
             'n' => 'No Especifica'
         ];
         return $genders[$gender];
+    }
+
+    public function edited($value, $key,  $column ,$rowId ){
+        $user = User::find($rowId);
+        $user->$column = $value;
+        $request = new Request($user->toArray());
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'lastnames' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'gender' => ['required', 'string', 'min:1' , 'max:1', 'in:m,f,n'],
+            // 'rut' => ['required', 'string', 'unique:users'],
+            'rut' => ['required', 'string', 'cl_rut'],
+            'phone' => ['required', 'string', 'max:255'],
+            'birthday' => ['required', 'date']
+        ]);
+        $user->save();
+        // ddd($request);
+
     }
 }
