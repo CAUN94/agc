94af6ecc2239d852fe906fe118e3b9fd4b64be23
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-01-31 15:47:48 -0300

    redirect

---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class RedirectController extends Controller {
+	public function index() {
+		$url = "https://youjustbetter.cl/";
+		return Redirect::away($url);
+
+	}
+
+	public function development() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLSd59ziGnReCKq0pNSzsjodtHgyui_hWrk7oZHKxsPavQUuPRQ/viewform";
+		return Redirect::away($url);
+
+	}
+
+	public function communications() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLSf-6LgbONv4BuFIzoyjDpHFM2EbBZVhHHMeP8P1XKUAOe8jlg/viewform";
+		return Redirect::away($url);
+
+	}
+
+	public function administration() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLSfI8R2A36mwtSwihtYocuQI9xgJ22L56E5PwvHN-ZFoqsC7Cw/viewform?usp=sf_link";
+		return Redirect::away($url);
+
+	}
+
+	public function whatsapp() {
+		$url = "https://api.whatsapp.com/send?phone=56933809726&text=Hola%20Equipo%20You";
+		return Redirect::away($url);
+	}
+
+	public function whatsappform(Request $request) {
+		$validated = $request->validate([
+			'message' => 'required',
+			'number' => 'required|min:8',
+		]);
+		$data = $request->all();
+		$number = substr($data['number'], -8);
+		$data['message'] = $data['message'] . " ";
+		$data['message'] = preg_replace('/\n+/', '%0A', $data['message']);
+		$data['message'] = preg_replace('/\s+/', '%20', $data['message']);
+		$url = "https://api.whatsapp.com/send?phone=569" . $number . "&text=" . $data['message'];
+		return Redirect::away($url);
+	}
+
+	public function trainning() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLScpUiedpwx-A20tpT_zVjineUTcGvpkHPpal7ztKekMTqP6TQ/viewform?usp=sf_link";
+		return Redirect::away($url);
+	}
+
+	public function arancel() {
+		$url = "https://docs.google.com/spreadsheets/d/1GeEQi1_4sWTI81nUj4z73SeKPBAh5VrFovGMASiLTMQ/edit?usp=sharing";
+		return Redirect::away($url);
+	}
+
+	public function pay() {
+		// $url = "https://pagatuprofesional.cl/profesionales/you-spa";
+		$url = "https://padpow.com/customer/professionals/3165/payments/new";
+		return Redirect::away($url);
+	}
+
+	public function contreras() {
+		$url = "https://meet.jit.si/dcontrerasb";
+		return Redirect::away($url);
+	}
+
+	public function barchiesi() {
+		$url = "https://meet.jit.si/rbarchiesiv";
+		return Redirect::away($url);
+	}
+
+	public function cristi() {
+		$url = "https://meet.jit.si/icristis";
+		return Redirect::away($url);
+	}
+
+	public function guzman() {
+		$url = "https://meet.jit.si/jmguzmanh";
+		return Redirect::away($url);
+	}
+
+	public function maldonado() {
+		$url = "https://meet.jit.si/amaldonados";
+		return Redirect::away($url);
+	}
+
+	public function martinez() {
+		$url = "https://meet.jit.si/mjmartinezm";
+		return Redirect::away($url);
+	}
+
+	public function moya() {
+		$url = "https://meet.jit.si/cmoyac";
+		return Redirect::away($url);
+	}
+
+	public function niklitschek() {
+		$url = "https://meet.jit.si/aniklitscheks";
+		return Redirect::away($url);
+	}
+
+	public function ross() {
+		$url = "https://meet.jit.si/mrossg";
+		return Redirect::away($url);
+	}
+
+	public function valenzuela() {
+		$url = "https://meet.jit.si/cvalenzuelar";
+		return Redirect::away($url);
+	}
+
+	public function vivallo() {
+		$url = "https://meet.jit.si/dvivallov";
+		return Redirect::away($url);
+	}
+
+	public function internos() {
+		$url = "https://meet.jit.si/internos";
+		return Redirect::away($url);
+	}
+
+	public function meetyou() {
+		$url = "https://meet.jit.si/meetyou";
+		return Redirect::away($url);
+	}
+
+	public function lopez() {
+		$url = "https://meet.jit.si/alopezm";
+		return Redirect::away($url);
+	}
+
+	public function fguzman() {
+		$url = "https://meet.jit.si/fguzmanh";
+		return Redirect::away($url);
+	}
+
+	public function hernandez() {
+		$url = "https://meet.jit.si/chernandezc";
+		return Redirect::away($url);
+	}
+
+	public function vitali() {
+		$url = "https://meet.jit.si/svitalimz";
+		return Redirect::away($url);
+	}
+
+	public function rrhh() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLSfp5FjEXouwK0_Tm6hkHaLMvrzqU6OsXmNhIYHUQ3-vdlbJSA/viewform?usp=sf_link";
+		return Redirect::away($url);
+	}
+
+	public function techo() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLSeRuajbx4B0Ox8fA3g9hRZfIdfdRLAwoU6eJIsec98XlTq2gA/viewform?usp=pp_url";
+		return Redirect::away($url);
+	}
+
+	public function rsf() {
+		$url = "https://docs.google.com/forms/d/e/1FAIpQLScCFgcGZG_OAlDUaG2VdAUUYD4m3dCHWcRD33cUl-zv2Wiw4A/viewform?usp=sf_link";
+		return Redirect::away($url);
+	}
+
+	public function registro() {
+		$url = "https://docs.google.com/forms/d/1NHVEMK_SuUTnFHyACwKxXvydQ0t9BhOLevnWaL3U9Bw/edit";
+		return Redirect::away($url);
+	}
+
+	public function comunicaciones() {
+		$url = "https://docs.google.com/document/d/19iDHBI6mONTLLZ1j0MZoxyc0cyixm_HZGp2SzRF3CIE/edit#heading=h.samqshe4j5m8";
+		return Redirect::away($url);
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,38 @@ Route::resource('adminclass', 'App\Http\Controllers\AdminTrainingController');
 Route::resource('adminprofessionals', 'App\Http\Controllers\AdminProfessionalsController');
 Route::resource('admintrainers', 'App\Http\Controllers\AdminTrainersController');
 
+Route::get('/padpow', 'App\Http\Controllers\RedirectController@pay');
+Route::get('/rsf', 'App\Http\Controllers\RedirectController@rsf');
+Route::get('/registro', 'App\Http\Controllers\RedirectController@registro');
+Route::get('/comunicaciones', 'App\Http\Controllers\RedirectController@comunicaciones');
+Route::get('/solicitud-desarrollo', 'App\Http\Controllers\RedirectController@development');
+Route::get('/solicitud-comunicaciones', 'App\Http\Controllers\RedirectController@communications');
+Route::get('/solicitud-administracion', 'App\Http\Controllers\RedirectController@administration');
+Route::get('/youphone', 'App\Http\Controllers\RedirectController@whatsapp');
+Route::post('/youphone_whatsapp', 'App\Http\Controllers\RedirectController@whatsappform');
+Route::get('/entrenamiento', 'App\Http\Controllers\RedirectController@trainning');
+Route::get('/aranceles', 'App\Http\Controllers\RedirectController@arancel');
+Route::get('/pago', 'App\Http\Controllers\RedirectController@pay');
+Route::get('/rrhh', 'App\Http\Controllers\RedirectController@rrhh');
+Route::get('/techosalud', 'App\Http\Controllers\RedirectController@techo');
+Route::get('/box/dcontrerasb', 'App\Http\Controllers\RedirectController@contreras');
+Route::get('/box/rbarchiesiv', 'App\Http\Controllers\RedirectController@barchiesi');
+Route::get('/box/icristis', 'App\Http\Controllers\RedirectController@cristi');
+Route::get('/box/jmguzmanh', 'App\Http\Controllers\RedirectController@guzman');
+Route::get('/box/amaldonados', 'App\Http\Controllers\RedirectController@maldonado');
+Route::get('/box/mjmartinezm', 'App\Http\Controllers\RedirectController@martinez');
+Route::get('/box/cmoyac', 'App\Http\Controllers\RedirectController@moya');
+Route::get('/box/aniklitscheks', 'App\Http\Controllers\RedirectController@niklitschek');
+Route::get('/box/mrossg', 'App\Http\Controllers\RedirectController@ross');
+Route::get('/box/cvalenzuelar', 'App\Http\Controllers\RedirectController@valenzuela');
+Route::get('/box/dvivallov', 'App\Http\Controllers\RedirectController@vivallo');
+Route::get('/box/internos', 'App\Http\Controllers\RedirectController@internos');
+Route::get('/box/alopezm', 'App\Http\Controllers\RedirectController@lopez');
+Route::get('/box/fguzmanh', 'App\Http\Controllers\RedirectController@fguzman');
+Route::get('/box/chernandezc', 'App\Http\Controllers\RedirectController@hernandez');
+Route::get('/box/svitalim', 'App\Http\Controllers\RedirectController@vitali');
+Route::get('/box/meetyou', 'App\Http\Controllers\RedirectController@meetyou');
+
 Route::get('change-password', 'App\Http\Controllers\ChangePasswordController@index');
 Route::post('change-password', 'App\Http\Controllers\ChangePasswordController@store')->name('change.password');
 
