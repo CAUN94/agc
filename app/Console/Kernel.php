e0fd00fe4c5c31ebe757b9366a8365298d24408d
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-04-28 16:22:53 -0400

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('ln:CheckRenew')->daily()->at('12:00');
+        $schedule->command('ln:CheckRenew')->daily()->at('16:30');
     }
 
     /**
