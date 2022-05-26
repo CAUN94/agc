0a698db716e3aff99efd1848952114792830b047
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-05-26 10:32:53 -0400

    changes

--- app/Http/Livewire/UserStravaPanel.php
+++ app/Http/Livewire/UserStravaPanel.php
@@ -105,7 +105,7 @@ class UserStravaPanel extends Component
               return $week->distance;
             }, $week));
         }
-        ddd($sumweek_distance);
+        // ddd($sumweek_distance);
         $last_weeks = array_slice($sumweek_distance, -3, 3, true);
 
         // return $last_weeks;
