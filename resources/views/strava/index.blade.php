6df45f4d12fb58e77e7204536566fb19b700d490
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-05-26 10:33:29 -0400

    changes

--- resources/views/strava/index.blade.php
+++ resources/views/strava/index.blade.php
@@ -2,7 +2,7 @@
     <div class="w-10/12 mx-auto text-white mt-10">
         <h2 class="text-lg mb-2">Lista de Usuarios</h2>
         @foreach($users as $user)
-            <ul class="w-1/3 ">
+            <ul class="w-1/3 mb-2">
                 <li class="flex items-center">
                     <img class="mr-10 rounded-full w-28" src="{{$user->avatar}}">
                     {{$user->username}}
