ad1c09607934737725ee992a96aa4ea2e16cac58
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-05-31 12:22:50 -0400

    changes

--- resources/views/components/landing2/header.blade.php
+++ resources/views/components/landing2/header.blade.php
@@ -1,14 +1,12 @@
 <div>
     <div class="my-10">
-        <h1 class="font-futura text-white font-medium text-center text-6xl uppercase">You Just <span class="text-primary-500">Better</span></h1>
+        <h1 class="font-futura text-white font-bold text-center text-6xl uppercase">You Just <span class="text-primary-500">Better</span></h1>
     </div>
 {{--     <div class="bg-primary-500" style="border-top-left-radius: 20%;border-top-right-radius: 20%;"> --}}
     <div class="bg-gradient-to-r from-you-grey to-primary-500">
         <div class="mx-auto grid grid-cols-1 justify-items-stretch container gap-y-10 pb-10 py-10 px-20 text-white text-xl">
             <p class="text-left">
                 Somos un centro integral de salud y medicina deportiva que promueve los estilos de vida saludables de la mano con especialistas del deporte y la medicina de estilos de vida.
-            </p>
-            <p class="text-left">
                 Entregamos un servicio de atención personalizada, basado en evidencia científica que busca fomentar tu proceso de autorrealización.
             </p>
         </div>
