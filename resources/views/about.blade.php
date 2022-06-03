fbc14a37ef409331aee2ac839340373506745ba9
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-06-03 16:19:29 -0400

    changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,14 +6,14 @@
                 </h1>
             </div>
         </header>
-        <h2 class="text-2xl text-light-grey font-futura font-bold text-center mt-10 -mb-12 uppercase">Kinesiología</h2>
+        <h2 class="text-2xl text-light-grey font-futura font-bold text-center mt-10 lg:-mb-12 uppercase">Kinesiología</h2>
         <x-landing2.call>
         </x-landing2.call>
-        <div class="grid grid-cols-3 mx-auto w-full container gap-4 mb-8">
+        <div class="grid grid-cols-0 lg:grid-cols-3 px-2 lg:px-0 mx-auto w-full container gap-4 mb-8">
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Terapía de masajes Hands-On</h2>
                 <h3 class="text-primary-500 text-xl">Incluye</h3>
-                <ul class="text-lg text-white list-disc list-inside">
+                <ul class="text-base lg:text-lg text-white list-disc list-inside">
                     <li class="mt-2">Masajes de tejido blando</li>
                     <li class="mt-2">Técnicas de estiramiento</li>
                     <li class="mt-2">Mobilización de tejido blando, manipulación y mobilización</li>
@@ -21,20 +21,20 @@
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Ejercicios en casa</h2>
-                <p class="text-lg text-white">
+                <p class="text-base lg:text-lg text-white">
                     Una parte fundamental del tratamiento son los <span class="text-primary-500">ejercicios en casa enseñados en YOU</span>, para reforzar los beneficios durante el tratamiento de kinesiología y el progeso de la recuperación.
                 </p>
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Punción Seca</h2>
-                <p class="text-lg text-white">
+                <p class="text-base lg:text-lg text-white">
                     Usado como parte del tratamiento de kinesiología para aliviar dolor en diversas condiciones y patologías; desde <span class="text-primary-500">dolores</span> de espalda baja hasta <span class="text-primary-500">artrosis</span>  de distintas <span class="text-primary-500">articulaciones.</span> Puede también ser benificioso  para <span class="text-primary-500">aliviar los puntos de gatillos miofasciales</span>.
                 </p>
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Taping</h2>
                 <h3 class="text-white text-xl">El tape puede ser usado en kinesiología para:</h3>
-                <ul class="text-lg text-white list-disc list-inside">
+                <ul class="text-base lg:text-lg text-white list-disc list-inside">
                     <li class="mt-2">Reducir hinchazón y <span class="text-primary-500">mejorar la circulación.</span></li>
                     <li class="mt-2"><span class="text-primary-500">Entregar estimulos propioceptivos.</span></li>
                     <li class="mt-2"><span class="text-primary-500">Reducir dolor.</span></li>
@@ -43,20 +43,20 @@
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Terapía de masajes deportivos</h2>
-                <p class="text-lg text-white">
+                <p class="text-base lg:text-lg text-white">
                     La terapia de masajes deportivos en la kinesiología deportiva es el manejo, <span class="text-primary-500">manipulación y rehabilitación de los tejidos blando</span> en el cuerpo, tales como: músculos, ligamentos y tendones. Usado para prevenir lesiones y aliviar estrés y tensiones. Además, beneficiará a cualquier persona que sufra estrés físico, laboral, emocional y postural.
                 </p>
             </div>
         </div>
         <div class="bg-primary-500">
-            <div class="grid grid-cols-4 mx-auto w-full container gap-4 mb-8 py-8">
+            <div class="grid mx-auto w-full py-4 lg:py-0 container gap-4 mb-8 lg:grid-cols-3 px-2 lg:px-0">
                 <div class="col-span-1">
                     <h2 class="text-2xl text-white">
                         Primera Visita en tu visita a Kinesiología:<br>¿Qué esperar?
                     </h2>
                 </div>
                 <div class="col-span-3">
-                    <p class="text-xl text-white">La evaluación inicial de la visita a kinesiología, toma hasta una hora. Está evaluación comprende desde la historia de tu condición física, deportiva y psicológica, una examinación exhaustiva de tu cuerpo completo, tratamientos y consejos para alcanzar tu máximo potencial. Para poder diagnosticar con la mayor precisión posible, puedes ser preguntado de quedar en ropa interior para ver el área afectada y las articulaciones arriba y abajo de esta. Sólo después de una completa evaluación y dedicando el tiempo para preguntas, el kinesiologo decidirá que es mejor para tu tratamiento.<br><br>Sí nuevos agendamientos son requeridos, estos también serán parte de la sesión de kinesiología, junto con un programa de tratamiento personalizado y planificado. El tiempo de está sesión también será para dedicar a enseñarte como evitar este patología recurrente. La revisión de tus nuevos agendamientos en kinesiología evaluaremos tu progreso, cómo también el tratamiento original aplicado.</p>
+                    <p class="text-base lg:text-lg text-white">La evaluación inicial de la visita a kinesiología, toma hasta una hora. Está evaluación comprende desde la historia de tu condición física, deportiva y psicológica, una examinación exhaustiva de tu cuerpo completo, tratamientos y consejos para alcanzar tu máximo potencial. Para poder diagnosticar con la mayor precisión posible, puedes ser preguntado de quedar en ropa interior para ver el área afectada y las articulaciones arriba y abajo de esta. Sólo después de una completa evaluación y dedicando el tiempo para preguntas, el kinesiologo decidirá que es mejor para tu tratamiento.<br><br>Sí nuevos agendamientos son requeridos, estos también serán parte de la sesión de kinesiología, junto con un programa de tratamiento personalizado y planificado. El tiempo de está sesión también será para dedicar a enseñarte como evitar este patología recurrente. La revisión de tus nuevos agendamientos en kinesiología evaluaremos tu progreso, cómo también el tratamiento original aplicado.</p>
                 </div>
             </div>
         </div>
