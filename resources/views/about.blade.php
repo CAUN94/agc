87e218576887bef6e8156cbfeda8e1fc1e2ab359
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-06-03 12:54:18 -0400

    changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,47 +9,43 @@
         <h2 class="text-2xl text-light-grey font-futura font-bold text-center mt-10 -mb-12 uppercase">Kinesiología</h2>
         <x-landing2.call>
         </x-landing2.call>
-        <div class="grid grid-cols-3 mx-auto w-full container gap-4 mb-4">
+        <div class="grid grid-cols-3 mx-auto w-full container gap-4 mb-8">
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Terapía de masajes Hands-On</h2>
                 <h3 class="text-primary-500 text-xl">Incluye</h3>
-                <ul>
-                    <li>Masajes de tejido blando</li>
-                    <li>Técnicas de estiramiento</li>
-                    <li>Mobilización de tejido blando, manipulación y mobilización</li>
+                <ul class="text-lg text-white list-disc list-inside">
+                    <li class="mt-2">Masajes de tejido blando</li>
+                    <li class="mt-2">Técnicas de estiramiento</li>
+                    <li class="mt-2">Mobilización de tejido blando, manipulación y mobilización</li>
                 </ul>
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Ejercicios en casa</h2>
-                <h3 class="text-primary-500 text-xl">Subtitle</h3>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
-                </ul>
+                <p class="text-lg text-white">
+                    Una parte fundamental del tratamiento son los <span class="text-primary-500">ejercicios en casa enseñados en YOU</span>, para reforzar los beneficios durante el tratamiento de kinesiología y el progeso de la recuperación.
+                </p>
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Punción Seca</h2>
-                <h3 class="text-primary-500 text-xl">Subtitle</h3>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
-                </ul>
+                <p class="text-lg text-white">
+                    Usado como parte del tratamiento de kinesiología para aliviar dolor en diversas condiciones y patologías; desde <span class="text-primary-500">dolores</span> de espalda baja hasta <span class="text-primary-500">artrosis</span>  de distintas <span class="text-primary-500">articulaciones.</span> Puede también ser benificioso  para <span class="text-primary-500">aliviar los puntos de gatillos miofasciales</span>.
+                </p>
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Taping</h2>
-                <h3 class="text-primary-500 text-xl">Subtitle</h3>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
+                <h3 class="text-white text-xl">El tape puede ser usado en kinesiología para:</h3>
+                <ul class="text-lg text-white list-disc list-inside">
+                    <li class="mt-2">Reducir hinchazón y <span class="text-primary-500">mejorar la circulación.</span></li>
+                    <li class="mt-2"><span class="text-primary-500">Entregar estimulos propioceptivos.</span></li>
+                    <li class="mt-2"><span class="text-primary-500">Reducir dolor.</span></li>
+                    <li class="mt-2">Mejorar la funcionalidad limitando o asistiendo a movimientos de articulaciones.</li>
                 </ul>
             </div>
             <div>
                 <h2 class="text-2xl text-primary-500 mb-5">Terapía de masajes deportivos</h2>
-                <h3 class="text-primary-500 text-xl">Subtitle</h3>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
-                </ul>
+                <p class="text-lg text-white">
+                    La terapia de masajes deportivos en la kinesiología deportiva es el manejo, <span class="text-primary-500">manipulación y rehabilitación de los tejidos blando</span> en el cuerpo, tales como: músculos, ligamentos y tendones. Usado para prevenir lesiones y aliviar estrés y tensiones. Además, beneficiará a cualquier persona que sufra estrés físico, laboral, emocional y postural.
+                </p>
             </div>
         </div>
         <div class="bg-primary-500">
