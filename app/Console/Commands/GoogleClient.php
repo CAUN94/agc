433c7e2860a552248241fdec4e0754187de9f7f1
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-07-13 11:31:06 -0400

    changes

---
+++ app/Console/Commands/GoogleClient.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Google\Client;
+use Google\Service\Calendar;
+
+class GoogleClient extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ln:googleClient';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Token Google';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $client = new Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes("https://www.googleapis.com/auth/calendar");
+        $client->setAuthConfig(public_path('credentials.json'));
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = public_path('token.json');
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+    }
+}
--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -4,21 +4,129 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Spatie\GoogleCalendar\Event;
+use Google\Client;
+use Google\Service\Calendar;
+use Google_Service_Calendar_Event;
 // use Spatie\IcalendarGenerator\Components\Calendar;
 // use Spatie\IcalendarGenerator\Components\Event as IEvent;
 // use Spatie\IcalendarGenerator\Properties\TextProperty;
 
 class CalendarController extends Controller
 {
-    public function google(){
-        $event = new Event;
+    public function getClient(){
+        $client = new Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes("https://www.googleapis.com/auth/calendar");
+        $client->setAuthConfig(public_path('credentials.json'));
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
 
-        $event->name = 'por fin funciona LPM';
-        $event->startDateTime = \Carbon\Carbon::now();
-        $event->endDateTime = \Carbon\Carbon::now()->addHour();
-        // $event->addAttendee(['email' => 'anotherEmail@gmail.com']);
-        $event->save();
-        return Event::get();
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = public_path('token.json');
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
+    }
+
+    public function google(){
+        $client = $this->getClient();
+        $service = new Calendar($client);
+        try{
+
+        $calendarId = 'primary';
+        $optParams = array(
+            'maxResults' => 10,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => date('c'),
+        );
+        $results = $service->events->listEvents($calendarId, $optParams);
+        $events = $results->getItems();
+
+        if (empty($events)) {
+            print "No upcoming events found.\n";
+        } else {
+            print "Upcoming events:\n";
+            foreach ($events as $event) {
+                echo "<br>";
+                $start = $event->start->dateTime;
+                if (empty($start)) {
+                    $start = $event->start->date;
+                }
+                printf("%s (%s) \n", $event->getSummary(), $start);
+
+            }
+        }
+    }
+    catch(Exception $e) {
+        // TODO(developer) - handle error appropriately
+        echo 'Message: ' .$e->getMessage();
+    }
+
+    $event = new Google_Service_Calendar_Event(array(
+      'summary' => 'Google I/O 2015',
+      'location' => '800 Howard St., San Francisco, CA 94103',
+      'description' => 'A chance to hear more about Google\'s developer products.',
+      'start' => array(
+        'dateTime' => '2022-07-17T17:00:00-06:00',
+        'timeZone' => 'America/Los_Angeles',
+      ),
+      'end' => array(
+        'dateTime' => '2022-07-17T17:00:00-07:00',
+        'timeZone' => 'America/Los_Angeles',
+      ),
+      'recurrence' => array(
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ),
+      'attendees' => array(
+        array('email' => 'cristobalugarte@gmail.com'),
+        array('email' => 'iver.cristi@gmail.com'),
+      ),
+      'reminders' => array(
+        'useDefault' => FALSE,
+        'overrides' => array(
+          array('method' => 'email', 'minutes' => 24 * 60),
+          array('method' => 'popup', 'minutes' => 10),
+        ),
+      ),
+    ));
+
+    $calendarId = 'primary';
+    $event = $service->events->insert($calendarId, $event);
+    printf('Event created: %s\n', $event->htmlLink);
     }
 
     // public function icalendar(){
