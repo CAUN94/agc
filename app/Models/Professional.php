1e94a6679194e874238e1388ff45e3b2db734172
Bwonsamdii <82678225+Bwonsamdii@users.noreply.github.com>
2022-08-31 12:42:22 -0400

    Creacion de los modulos nextAppointments, monthAppointments y remuneraciones

--- app/Models/Professional.php
+++ app/Models/Professional.php
@@ -14,4 +14,22 @@ class Professional extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public static function nextAppointments(){
+        return AppointmentMl::where('Hora_inicio','>=',\Carbon\Carbon::now('America/Santiago')->format('H:i:s'))
+                            ->where('Fecha','>=',\Carbon\Carbon::now('America/Santiago')->format('Y-m-d'))
+                            ->where('Estado', '!=', 'Anulado')
+                            ->orderby('Hora_inicio','ASC')
+                            ->get();
+    }
+
+    public static function monthAppointments($first,$last){
+       //where('Profesional' , '=', 'Alonso Niklitschek Sanhueza')
+        return ActionMl::whereBetween('Fecha_Realizacion',[$first, $last])
+        ->get();
+    }
+
+    public static function remuneracion($first,$last){
+      return (ActionMl::whereBetween('Fecha_Realizacion',[$first, $last])->sum('Precio_Prestacion'))*1;
+    }
 }
