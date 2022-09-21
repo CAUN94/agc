af40d638c6eaecb9adc34ee37bb6a50aa56e058b
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-09-21 13:12:54 -0300

    Calendar Update

--- app/Console/Commands/GoogleCalendarDelete.php
+++ app/Console/Commands/GoogleCalendarDelete.php
@@ -44,9 +44,11 @@ class GoogleCalendarDelete extends Command
     public function handle()
     {
         $this->getClient();
-        $this->superdelete('Alonso Niklitschek Sanhueza','c_1hkcfsu55r04nisn1b087b4f5g@group.calendar.google.com');
-        $this->superdelete('Jaime Pantoja Rodriguez','c_1dhlgacu9kmin254ievq27cp7s@group.calendar.google.com');
-        $this->superdelete('Daniella Vivallo Vera','c_jur7p65ntosbr42ghs94cnj3no@group.calendar.google.com');
+        $this->superdelete('Alonso Niklitschek Sanhueza','c_0f1ec4cb6ddb69027c0f6e89110fb1708a9613a28be7efc2165475004be2398d@group.calendar.google.com');
+        $this->superdelete('Jaime Pantoja Rodriguez','c_a5e903612c38cf636cddc1ad9f5d85f5cfc58fc2350ab902696cef768930fe27@group.calendar.google.com');
+        $this->superdelete('Daniella Vivallo Vera','c_70cc091402f167e8244d73d4a32ad15d0e21368ead3e7145eede9f69ce57b019@group.calendar.google.com');
+        $this->superdelete('Manuel Silva Ávila','c_cdc49b01298351d88c6468980988cf46b288841aeabf875ebffaed4a06b45530@group.calendar.google.com');
+        $this->superdelete('Camila Valentini Rojas','c_774a51d77aa92fff83bc27c54aea7caa6214d02b32415d38e38833e2bf6c88ea@group.calendar.google.com');
     }
 
 
