f8f23df7b89efbe049d4ba15789ca37c05cb01bb
Bwonsamdii <82678225+Bwonsamdii@users.noreply.github.com>
2022-12-18 23:05:44 -0400

    Cambios y Actualizacion de panel de remuneraciones y adicion de las columnas de coefficiente y horas semana a la base de datos

---
+++ database/migrations/2022_12_08_163752_add_two_column_to_professionals.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTwoColumnToProfessionals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('professionals', function (Blueprint $table) {
+            $table->float('coeff')->after('class');
+            $table->integer('Horas_disponible')->after('coeff');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('professionals', function (Blueprint $table) {
+            $table->dropColumn('coeff');
+            $table->dropColumn('Horas_disponible');
+        });
+    }
+}
