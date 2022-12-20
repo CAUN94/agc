7f9aef73c64965e3f2ef207fdf4f8a2089473087
cugarte94 <cugarte@guiasyscoutschile.cl>
2022-12-20 12:44:58 -0300

    update

--- app/Http/Controllers/AdminMedilinkController.php
+++ app/Http/Controllers/AdminMedilinkController.php
@@ -67,7 +67,7 @@ class AdminMedilinkController extends Controller
                 // if($i >= 60){
                 //     break;
                 // }
-                sleep(0.2);
+                sleep(0.1);
                 if($data->fecha >= '2022-11-20'){
                     continue;
                 }
