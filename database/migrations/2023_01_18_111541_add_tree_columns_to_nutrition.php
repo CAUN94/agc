b3a61100ef624ea0f1186be7c6ad77a1b0d8fdc5
cugarte94 <cugarte@guiasyscoutschile.cl>
2023-01-19 10:39:31 -0300

    nutrition

--- database/migrations/2023_01_18_111541_add_tree_columns_to_nutrition.php
+++ database/migrations/2023_01_18_111541_add_tree_columns_to_nutrition.php
@@ -14,9 +14,9 @@ class AddTreeColumnsToNutrition extends Migration
     public function up()
     {
         Schema::table('nutrition', function (Blueprint $table) {
-            $table->float('masa_adiposa_porc')->after('masa_adiposa')->default(1);
-            $table->float('masa_muscular_porc')->after('masa_muscular')->default(1);
-            $table->float('masa_osea_porc')->after('masa_osea')->default(1);
+            // $table->float('masa_adiposa_porc')->after('masa_adiposa')->default(1);
+            // $table->float('masa_muscular_porc')->after('masa_muscular')->default(1);
+            // $table->float('masa_osea_porc')->after('masa_osea')->default(1);
         });
     }
 
