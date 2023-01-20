f920ce8608b1aac867b747e0b8c31a559ca0eb36
cugarte94 <cugarte@guiasyscoutschile.cl>
2023-01-20 14:20:17 -0300

    remuneration $

--- database/migrations/2022_09_06_163750_add_two_column_to_professionals_2.php
+++ database/migrations/2022_09_06_163750_add_two_column_to_professionals_2.php
@@ -14,7 +14,7 @@ class AddTwoColumnToProfessionals2 extends Migration
     public function up()
     {
         Schema::table('professionals', function (Blueprint $table) {
-            $table->string('mail')->after('description');
+            // $table->string('mail')->after('description');
             $table->text('class')->after('mail');
         });
     }
