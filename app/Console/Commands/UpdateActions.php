ba64c689ee8cc75313a9be8bd3902ead676ce03c
cugarte94 <cugarte@guiasyscoutschile.cl>
2023-01-23 10:51:38 -0300

    remuneration $

--- app/Console/Commands/UpdateActions.php
+++ app/Console/Commands/UpdateActions.php
@@ -6,6 +6,11 @@ use Illuminate\Console\Command;
 use Carbon\Carbon;
 use App\Models\ActionMl;
 use App\Models\AppointmentMl;
+use App\Models\User;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class UpdateActions extends Command
 {
@@ -45,82 +50,137 @@ class UpdateActions extends Command
     }
 
     public function allActions(){
+
+        $id = 48;
         $client = new \GuzzleHttp\Client();
+        $url = 'https://api.medilink.healthatom.com/api/v1/profesionales/'.$id;
+        $response = $client->request('GET', $url, [
+            'headers'  => [
+                'Authorization' => 'Token ' . $this->token
+            ]
+        ]);
 
-        $allAtentions = AppointmentMl::where('Fecha','>','2022-12-20')
-        ->where('Profesional','like','%Cons%')
-        ->where('Estado','Atendido')->get();
-        $this->info($allAtentions->count());
-        $count = 1;
-        foreach($allAtentions as $ar){
-            $this->info($ar->Tratamiento_Nr);
-            $url = 'https://api.medilink.healthatom.com/api/v1/atenciones/'.$ar->Tratamiento_Nr;
+        $rut = json_decode($response->getBody())->data->rut;
+        $coff = User::where('rut',$rut)->first()->professional->coeff;
 
-            $response = $client->request('GET', $url, [
-                'headers'  => [
-                    'Authorization' => 'Token ' . $this->token
-                ]
-            ]);
+        $client = new \GuzzleHttp\Client();
+        $url = 'https://api.medilink.healthatom.com/api/v1/profesionales/'.$id.'/citas?q={"fecha":{"gt":"2022-12-20"},"estado_cita":{"eq":"Atendido"}}&sort=fecha:desc';
 
-            $atention = json_decode($response->getBody())->data;
+        $response = $client->request('GET', $url, [
+            'headers'  => [
+                'Authorization' => 'Token ' . $this->token
+            ]
+        ]);
 
-            if($ar->Tratamiento_Nr == 22811){
-                $this->info(var_dump($atention));
-            }
+        $body = json_decode($response->getBody());
+        $appointments = [];
+        $pays = [];
+        $total_final = 0;
+        // $i = 0;
+        while(True){
+            foreach($body->data as $j => $data){
+                // $i++;
+                // if($i >= 60){
+                //     break;
+                // }
+                sleep(0.3);
+                if($data->fecha < '2022-12-21' or $data->fecha > '2023-01-20'){
+                    continue;
+                }
+                $id_atencion = $data->id_atencion;
+                $client = new \GuzzleHttp\Client();
+                $url = 'https://api.medilink.healthatom.com/api/v1/atenciones/'.$id_atencion.'/detalles';
 
-            $url = 'https://api.medilink.healthatom.com/api/v1/atenciones/'.$ar->Tratamiento_Nr.'/detalles';
-
-            $response = $client->request('GET', $url, [
-                'headers'  => [
-                    'Authorization' => 'Token ' . $this->token
-                ]
-            ]);
-    
-            $actions = json_decode($response->getBody())->data;
-    
-            $url = 'https://api.medilink.healthatom.com/api/v1/atenciones/'.$ar->Tratamiento_Nr.'/citas';
-            
-    
-            $response = $client->request('GET', $url, [
-                'headers'  => [
-                    'Authorization' => 'Token ' . $this->token
-                ]
-            ]);
-    
-            $cita = json_decode($response->getBody())->data[0];
-    
-            $estado = $cita->estado_cita;
-            
-            foreach($actions as $action){
-                $nombre = strtok($atention->nombre_paciente,  ' ');
-                $apellido = substr($atention->nombre_paciente, strpos($atention->nombre_paciente, " ") + 1);
-      
-                $new_row = actionMl::updateOrCreate([
-                  'Tratamiento_Nr'=> $atention->id,
-                  'Prestacion_Nr'=> $action->id_prestacion,
-                ],[
-                  'Sucursal'=> $atention->nombre_sucursal,
-                  'Nombre'=>$nombre,
-                  'Apellido'=>$apellido,
-                  'Categoria_Nr' => $atention->id_tipo,
-                  'Categoria_Nombre'=> $atention->nombre_tipo,
-                  'Tratamiento_Nr'=> $atention->id,
-                  'Profesional'=> $atention->nombre_profesional,
-                  'Estado'=> $estado,
-                  'Convenio'=> $atention->nombre_convenio,
-                  'Prestacion_Nr'=> $action->id_prestacion,
-                  'Prestacion_Nombre'=> $action->nombre_prestacion,
-                  'Fecha_Realizacion'=> $atention->fecha,
-                  'Precio_Prestacion'=> $atention->total,
-                  'Abono'=> $atention->abonado,
-                  'Total'=> $atention->total_realizado,
-                  'created_at'=> Carbon::Now(),
-                  'updated_at'=> Carbon::Now()
+                $response = $client->request('GET', $url, [
+                    'headers'  => [
+                        'Authorization' => 'Token ' . $this->token
+                    ]
                 ]);
-                $this->info($count.") ".$new_row->Profesional." ".$new_row->Fecha_Realizacion);
-            }
+                $pay = json_decode($response->getBody());
+                // $total += $pay->data->total;
+                $total = 0;
+                foreach($pay->data as $i => $data_pay){
+                    if($data_pay->total === 0 or !isset($data_pay->total)){
+                        $client = new \GuzzleHttp\Client();
 
-            $count++;
+                        $url = 'https://api.medilink.healthatom.com/api/v1/prestaciones/'.$data_pay->id_prestacion;
+                        $response = $client->request('GET', $url, [
+                            'headers'  => [
+                                'Authorization' => 'Token ' . $this->token
+                            ]
+                        ]);
+                        $prestacion = json_decode($response->getBody())->data;
+                        // ddd($data_pay);
+                        $total += $data_pay->subtotal;
+                        $pay->data[$i]->total = $data_pay->subtotal;
+
+
+                    } else {
+                        $total += $data_pay->total;
+                    }
+
+                }
+                if(count($pay->data) == 0){
+                    $total += 23990;
+                    $pay = ['pagado' => 0, 'total' => 23990];
+                    $pay = (object)$pay;
+                    $pay = [$pay];
+                } else {
+                    $pay = $pay->data;
+                }
+
+                $total_final += $total;
+                $pays[] = [$data, $pay,$total];
+                // $pays[] = [$data, $pay->data];
+
+            }
+            if(!isset($body->links->next)){
+                break;
+            }
+            $url = $body->links->next;
+            $response = $client->request('GET', $url, [
+                'headers'  => [
+                    'Authorization' => 'Token ' . $this->token
+                ]
+            ]);
+            $body = json_decode($response->getBody());
         }
+        // return $total;
+
+        foreach($pays as $pay){
+            $this->info($pay[0]->nombre_paciente);
+            foreach($pay[1] as $action){
+                if(!isset($action->id_prestacion)){
+                    $action->id_prestacion = $pay[0]->nombre_atencion;
+                    $action->nombre_prestacion = $pay[0]->nombre_atencion;
+                    $action->subtotal = $action->total;
+                }
+                $new_row = actionMl::updateOrCreate([
+                    'Tratamiento_Nr'=> $pay[0]->id_atencion,
+                    'Prestacion_Nr'=> $action->id_prestacion,
+                ],[
+                    'Sucursal'=> $pay[0]->nombre_sucursal,
+                    'Nombre'=>$pay[0]->nombre_paciente,
+                    'Apellido'=>'',
+                    'Categoria_Nr' => $pay[0]->nombre_atencion,
+                    'Categoria_Nombre'=> $pay[0]->nombre_atencion,
+                    'Tratamiento_Nr'=> $pay[0]->id_atencion,
+                    'Profesional'=> $pay[0]->nombre_profesional,
+                    'Estado'=> $pay[0]->estado_cita,
+                    'Convenio'=> '',
+                    'Prestacion_Nr'=> $action->id_prestacion,
+                    'Prestacion_Nombre'=> $action->nombre_prestacion,
+                    'Fecha_Realizacion'=> $pay[0]->fecha,
+                    'Precio_Prestacion'=> $action->subtotal,
+                    'Abono'=> $action->pagado,
+                    'Total'=> $action->total,
+                    'created_at'=> Carbon::Now(),
+                    'updated_at'=> Carbon::Now()
+                ]);
+            }
+        }
+        
+        $this->info(count($pays));
     }
+   
 }
