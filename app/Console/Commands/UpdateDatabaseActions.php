1878f71462320ac41cf69a683a44ca77004adc05
cugarte94 <cugarte@guiasyscoutschile.cl>
2023-02-21 11:30:09 -0300

    remunerations

--- app/Console/Commands/UpdateDatabaseActions.php
+++ app/Console/Commands/UpdateDatabaseActions.php
@@ -77,7 +77,7 @@ class UpdateDatabaseActions extends Command
       $count = 0;
       // Carbon Yesterday Date
       
-      $allAtentions = AppointmentMl::where('Fecha','>',Carbon::now()->subdays(60)
+      $allAtentions = AppointmentMl::where('Fecha','>',Carbon::now()->subdays(30)
         ->format('Y-m-d'))->whereIn('Estado',['Atendido','Atendiéndose'])->get();
 
       foreach($allAtentions as $ar){
