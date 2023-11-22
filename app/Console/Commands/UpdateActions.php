f500847eb7c6f661f0e275cc5db21df2eb3073c3
cugarte94 <cugarte@guiasyscoutschile.cl>
2023-11-22 17:05:44 -0300

    updates

--- app/Console/Commands/UpdateActions.php
+++ app/Console/Commands/UpdateActions.php
@@ -189,7 +189,7 @@ class UpdateActions extends Command
                 // First action where tratamiento_nr and prestacion_nr or continue
                 $checkAction = ActionMl::where('Tratamiento_Nr',$pay[0]->id_atencion)->first();
                 $this->info("Valor de ",isset($checkAction));
-                if(isset($checkAction)) {
+                if(!is_null($checkAction)) {
                     if($checkAction->Precio_Prestacion == 0){
                         // No se modifico la action
                         $this->info('No se modifico la action '.$checkAction->id);
