f8a1fea556c64547353313732735b390e15992b0
cugarte94 <cugarte@guiasyscoutschile.cl>
2023-11-28 17:15:38 -0300

    train

--- app/Http/Livewire/TrainAppointmentComponent.php
+++ app/Http/Livewire/TrainAppointmentComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Student;
 use Livewire\Component;
 use App\Models\TrainAppointment;
 use App\Models\Training;
@@ -12,8 +13,13 @@ use Carbon\Carbon;
 class TrainAppointmentComponent extends Component
 {
     public $trainAppointments;
+    public $trainAppointment;
+    public $trainBooks;
+    public $train;
     public $users;
+    public $students;
     public $searchTermUser;
+    public $searchTermStudent;
     public $selectedUserId;
     public $currentWeekTrainAppointments;
 
@@ -21,14 +27,43 @@ class TrainAppointmentComponent extends Component
     {
         // All users raw db concat user name and rut
         $this->users = User::selectRaw('CONCAT(name, " ", lastnames, " ", rut) as full_name, id')->get();
+
+        // AllStudents group by user_id
+        $this->students = User::join('students', 'users.id', '=', 'students.user_id')->selectRaw('CONCAT(name, " ", lastnames, " ", rut) as full_name, users.id')->groupby('users.id')->get();
         
         // Obtener las clases de la actual semana
         $now = Carbon::now();
         $this->currentWeekTrainAppointments = TrainAppointment::whereBetween('date', [$now->startOfWeek(), $now->endOfWeek()])->get();
+        $this->trainBooks = [];
     }
 
-    public function selectUser($id){
-        $this->user = User::find($id);
+    public function selectStudent($id){
+        $user = User::find($id);
+        $trainBook = new TrainBook();
+        $trainBook->user_id = $user->id;
+        $trainBook->train_appointment_id = $this->train;
+        $trainBook->status = 1;
+        $trainBook->save();
+        $this->trainBooks = TrainAppointment::find($this->train)->Bookings;
+        $this->searchTermStudent = '';
+    }
+
+    public function selectTrainAppointment($id){
+        $this->train = $id;
+        $this->trainBooks = TrainAppointment::find($id)->Bookings;
+    }
+
+    public function status($id){
+        $trainBook = TrainBook::find($id);
+        $trainBook->status = !$trainBook->status;
+        $trainBook->save();
+        $this->trainBooks = TrainAppointment::find($trainBook->train_appointment_id)->Bookings;
+    }
+
+    public function delete($id){
+        $trainBook = TrainBook::find($id);
+        $trainBook->delete();
+        $this->trainBooks = TrainAppointment::find($trainBook->train_appointment_id)->Bookings;
     }
 
     public function render()
@@ -40,10 +75,18 @@ class TrainAppointmentComponent extends Component
             $ids = User::search($this->searchTermUser)->get()->pluck('id');
             $this->users = User::selectRaw('CONCAT(name, " ", lastnames, " ", rut) as full_name, id')->whereIn('id', $ids)->get();
         }
-        // All train appointments from this week
-        $this->trainAppointments = TrainAppointment::join('train_appointments_pivot', 'train_appointments.id', '=', 'train_appointments_pivot.train_appointment_id')->join('trainings', 'train_appointments_pivot.training_id', '=', 'trainings.id')->where('type', 'Group')->whereDate('date', '2023-11-27')->groupby('train_appointments.id')->get();
-    
-            
+
+        if (empty($this->searchTermStudent)) {
+            $this->students = '';
+        }
+        else {
+            $ids = User::search($this->searchTermStudent)->get()->pluck('id');
+            $this->students = User::join('students', 'users.id', '=', 'students.user_id')->selectRaw('CONCAT(name, " ", lastnames, " ", rut) as full_name, users.id')->whereIn('users.id', $ids)->groupby('users.id')->get();
+        }
+        
+        $this->trainAppointments = TrainAppointment::join('train_appointments_pivot', 'train_appointments.id', '=', 'train_appointments_pivot.train_appointment_id')->join('trainings', 'train_appointments_pivot.training_id', '=', 'trainings.id')->where('type', 'Group')->whereDate('date', '2023-11-27')->groupby('train_appointments.id')
+        ->orderBy('date', 'asc')->orderBy('hour', 'asc')
+        ->get();
         return view('livewire.train-appointment-component');
     }
 
--- resources/views/livewire/train-appointment-component.blade.php
+++ resources/views/livewire/train-appointment-component.blade.php
@@ -1,36 +1,83 @@
+<div>
 <div class="mt-5 md:mt-0 md:col-span-3">
-        <div class="shadow sm:rounded-md sm:overflow-hidden my-5">
-            <div>
-                <div class="bg-white shadow overflow-hidden sm:rounded-t-md">
-                  <div class="border-t border-gray-200">
-                    <dl>
-                      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-                        <dd class="text-sm font-medium text-gray-500">
-                            <div class="col-span-4 sm:col-span-4">
-                                <input type="text" name="user" class="mt-1 focus:ring-primary-500 focus:border-primary-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" wire:model="searchTermUser" placeholder="Buscar Usuario">
-                            </div>
-                            @if($this->users && $this->users->count() > 0)
-                            <ul class="border border-gray-200 rounded overflow-hidden shadow-md col-span-6 absolute z-10">
-                                @foreach($this->users as $user)
-                                <li wire:click="selectUser({{$user->id}})" class="px-4 py-2 bg-white  hover:bg-primary-100 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
-                                  {{$user->full_name}}
-                                </li>
+    <div class="shadow sm:rounded-md sm:overflow-hidden my-5">
+        <div>
+            <div class="bg-white shadow overflow-hidden sm:rounded-t-md">
+              <div class="border-t border-gray-200">
+                <dl>
+                  <div class="bg-white px-4 gap-10 sm:grid sm:grid-cols-1 sm:gap-6 sm:px-6 py-5">
+                    <dd class="text-sm font-medium text-gray-500 py-1 sm:py-0">
+                        <div class="col-span-4 sm:col-span-4">
+                            <!-- select with live wire on select with $this->trainAppointments foreach -->
+                            <select wire:model="trainAppointment" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm">
+                                <option value="">Seleccionar Clase</option>
+                                @foreach($this->trainAppointments as $trainAppointment)
+                                <option wire:click="selectTrainAppointment({{$trainAppointment->train_appointment_id}})">{{$trainAppointment->name}} - {{$trainAppointment->date}} - {{$trainAppointment->hour}}</option>
                                 @endforeach
-                            </ul>
-                            @endif
-                        </dd>
-                      </div>
-                    </dl>
+                            </select>
+                        </div>
+                    </dd>
                   </div>
-                </div>
+                </dl>
+              </div>
+            </div>
+            
+        </div>
+    </div>
+    
+</div>
 
+@if($this->trainBooks)
+  <div class="mt-5 md:mt-0">
+    <div class="shadow sm:rounded-md sm:overflow-hidden my-5">
+        <div>
+            <div class="bg-white shadow overflow-hidden sm:rounded-t-md px-6 py-5">
+              <div>
+                  <div class="col-span-4 sm:col-span-4">
+                    <input type="text" name="user" class="mt-1 focus:ring-primary-500 focus:border-primary-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" wire:model="searchTermStudent"  placeholder="Buscar Alumno">
+                  </div>
+                  @if($this->students && $this->students->count() > 0)
+                  <ul class="border border-gray-200 rounded overflow-hidden shadow-md col-span-6 absolute z-10">
+                      @foreach($this->students as $student)
+                      <li wire:click="selectStudent({{$student->id}})" class="px-4 py-2 bg-white  hover:bg-primary-100 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                        {{$student->full_name}}
+                      </li>
+                      @endforeach
+                  </ul>
+                  @endif
+              </div>
+                
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="shadow sm:rounded-md sm:overflow-hidden my-5">
+        <div>
+            <div class="bg-white shadow overflow-hidden sm:rounded-t-md px-6 py-5">
+              <div>
+                  <ul>
+                  @foreach($this->trainBooks as $trainBook)
+                    <li>
+                      {{$trainBook->user->name}}
+                      {{$trainBook->user->lastnames}}
+                      {{$trainBook->user->rut}}
+                      <!-- Div Flex col with two span with wireclicks Asistio/No Asistio and spann with Borrar -->
+                      @if($trainBook->status == 0)
+                      <span class="text-primary-500" wire:click="status({{$trainBook->id}})">Asistio</span>
+                      @else
+                      <span class="text-red-500" wire:click="status({{$trainBook->id}})">No Asistio</span>
+                      @endif
+                      <span class="text-yellow-500" wire:click="delete({{$trainBook->id}})">Borrar</span>
+                    </li>
+                  @endforeach
+                  </ul>
+                
+              </div>
             </div>
         </div>
-</div>
+    </div>
+  </div>
+@endif
 
-<div class="mt-5 md_mt-0">
-    @foreach($this->trainAppointments as $trainAppointment)
-      {{$trainAppointment->name}} {{$trainAppointment->date}} {{$trainAppointment->hour}}<br>
-    @endforeach
-</div>
 
+</div>
