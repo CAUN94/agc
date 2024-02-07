294be6ece41b333a24b07345b805e130648cbe25
cugarte94 <cugarte@guiasyscoutschile.cl>
2024-02-07 15:53:48 -0300

    contactanos

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -9,25 +9,19 @@
                 <button class="text-base sm:text-3xl top-2/3 bg-green-500 text-white py-2 sm:py-3 px-4 sm:px-6 rounded hover:bg-green-700 w-full sm:w-2/3">Consulta por convenio alianza o bono fonasa <i class="fab fa-whatsapp hidden sm:inline"></i></button>
             </div>
         </a> 
-        <!-- href to mail you@justbetter.cl with message "Hola Equipo You" -->
         <a href="mailto:you@justbetter.cl?subject=Hola%20Equipo%20You" target="_blank">
             <div class="flex items-center justify-around mt-5">
                 <button class="text-base sm:text-3xl top-2/3 bg-primary-500 text-white py-2 sm:py-3 px-4 sm:px-6 rounded hover:bg-primary-900 w-full sm:w-2/3">Mailing <i class="fas fa-envelope hidden sm:inline"></i></button>
             </div>
         </a>
-        <!-- href call to "+56933809726" -->
         <a href="tel:+56933809726" target="_blank">
             <div class="flex items center justify-around mt-5">
                 <button class="text-base sm:text-3xl top-2/3 bg-red-500 text-white py-2 sm:py-3 px-4 sm:px-6 rounded hover:bg-red-900 w-full sm:w-2/3">Llámanos <i class="fas fa-phone hidden sm:inline"></i></button>
             </div>
         </a>
-
-        <!-- Instagram  -->
         <a href="https://www.instagram.com/you.justbetter/" target="_blank">
             <div class="flex items-center justify-around mt-5">
                 <button class="text-base sm:text-3xl top-2/3 bg-pink-500 text-white py-2 sm:py-3 px-4 sm:px-6 rounded hover:bg-pink-900 w-full sm:w-2/3">Instagram <i class="fab fa-instagram hidden sm:inline"></i></button>
             </div>
         </a>
-
-
 </x-landing.layout>
