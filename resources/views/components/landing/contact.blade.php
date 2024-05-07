da4d415f84def1a355af71bb65606c975b99b531
cugarte94 <cugarte@guiasyscoutschile.cl>
2024-05-07 11:35:31 -0400

    all

--- resources/views/components/landing/contact.blade.php
+++ resources/views/components/landing/contact.blade.php
@@ -8,7 +8,7 @@
         <a
         class="text-center"
         target="_blank"
-        href="https://www.google.com/maps/dir//San+Pascual+736+Las+Condes+Regi%C3%B3n+Metropolitana/@-33.4178579,-70.5775512,16z/data=!4m5!4m4!1m0!1m2!1m1!1s0x9662cf1e8273f745:0x65dbdf4e0b964b33">
+        href="https://www.google.com/maps/dir//San+Pascual+309+Las+Condes+Regi%C3%B3n+Metropolitana/@-33.4178579,-70.5775512,16z/data=!4m5!4m4!1m0!1m2!1m1!1s0x9662cf1e8273f745:0x65dbdf4e0b964b33">
             <i class="fas fa-map-marker-alt"></i>
             San Pascual 309, Las Condes, Santiago
         </a>
@@ -36,7 +36,7 @@
     x-init="$interval(addOne, timer2)" x-cloack>
     <div class="relative">
         <section class="absolute bottom-0 right-0">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.2065919308297!2d-70.57973988482027!3d-33.41785788078324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf1e8273f745%3A0x65dbdf4e0b964b33!2sSan%20Pascual%20736%2C%20Las%20Condes%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1633367044424!5m2!1ses-419!2scl" class="w-32 lg:w-64 h-20 lg:h-36 " style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.2894972526437!2d-70.58236652434098!3d-33.4156960734033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf1ed5b6a927%3A0xc3dc6ff7676fa96!2sSan%20Pascual%20309%2C%207580159%20Las%20Condes%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1715095785953!5m2!1ses-419!2scl" class="w-32 lg:w-64 h-20 lg:h-36 " style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </section>
         <template x-for="(img,index) in imgs">
             <div class="img-slider"
